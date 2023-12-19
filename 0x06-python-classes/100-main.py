c79613d8bf3f707530522a13af3663f94337e3d1
edwkpy <141271186+edwkpy@users.noreply.github.com>
2023-12-19 20:52:39 +0300

    100-main.py

--- 0x06-python-classes/100-main.py
+++ 0x06-python-classes/100-main.py
@@ -1,4 +1,5 @@
-inglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
+#!/usr/bin/python3
+SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
 
 sll = SinglyLinkedList()
 sll.sorted_insert(2)
