1e492e94ec22a4348d2fe05c03012cc314e3feb3
edwkpy <141271186+edwkpy@users.noreply.github.com>
2023-12-19 21:10:07 +0300

    2-main.py

--- 0x06-python-classes/2-main.py
+++ 0x06-python-classes/2-main.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
-Square = __import__('2-square').Square
+"""
+2. Size validation
+A class Square that defines a square
+"""
 
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
 
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+class Square:
+    """Class that defines a square"""
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+    def __init__(self, size=0):
+        """
+        Define private instance attribute: size
+        Raise TypeError & ValueError if not int or <0 resp.
+        """
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
 
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
+        if size < 0:
+            raise ValueError('size must be >= 0')
 
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
+        self.__size = size
